08bbd55e52612f37a1de9f777cf7eb6524a8833f
Michael Parvis <michaelparvis@Michaels-MacBook-Pro.local>
2018-04-25 00:36:44 +0100

    containers and reducers initial

---
+++ src/reducers/reducerBooks.js
@@ -0,0 +1,11 @@
+export default function () {
+	return [
+		{ title: 'Harry Potter' },
+		{ title: 'Lord of the Rings' },
+		{ title: 'Lord of the Flies' },
+		{ title: 'Stone Cold' },
+		{ title: 'The Stand' },
+		{ title: 'Misery' },
+		{ title: 'The Road' }
+	]
+}
