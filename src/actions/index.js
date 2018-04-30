8961a1b798379a8751ef8ecd47155fd4adb80227
Michael Parvis <michaelparvis@Michaels-MacBook-Pro.local>
2018-04-30 17:58:04 +0100

    reducer addition

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ export function selectBook(book) {
 	// selectBook is an action creator, it returns an action
 	// an action with a type property
 	return {
-
+	 type: 'BOOK_SELECTED',
+	 payload: book
 	};
 }
