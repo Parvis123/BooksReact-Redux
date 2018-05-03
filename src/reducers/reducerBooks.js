17fff719e101d2856f744a2dbf12d3a32a4130d3
Michael Parvis <michaelparvis@Michaels-MacBook-Pro.local>
2018-05-03 16:44:31 +0100

    detail reducers and components

--- src/reducers/reducerBooks.js
+++ src/reducers/reducerBooks.js
@@ -1,11 +1,11 @@
 export default function () {
 	return [
-		{ title: 'Harry Potter' },
-		{ title: 'Lord of the Rings' },
-		{ title: 'Lord of the Flies' },
-		{ title: 'Stone Cold' },
-		{ title: 'The Stand' },
-		{ title: 'Misery' },
-		{ title: 'The Road' }
+		{ title: 'Harry Potter and the Half Blood Prince', pages: 607  },
+		{ title: 'The Two Towers', pages: 415},
+		{ title: 'Lord of the Flies', pages: 248 },
+		{ title: 'Stone Cold', pages: 132 },
+		{ title: 'The Stand', pages:823 },
+		{ title: 'Misery', pages:420 },
+		{ title: 'The Road', pages:287 }
 	]
 }
